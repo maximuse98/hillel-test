e81bfa4628756a65b628ea76217e053cafa62627
maxim.kabakov <maxim.kabakov@hys-enterprise.com>
2023-04-26 20:26:03 +0300

    Create square function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Square(2));
+        Console.WriteLine(Square(10));
+    }
+
+    public static int Square(int n)
+    {
+        return (int)Math.Pow(n, 2);
+    }
+}
