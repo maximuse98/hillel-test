3ca055ff2eafcee05fe871c842665855359317b6
Aleksey Monakhov <goshagriboedskii@gmail.com>
2023-04-29 15:20:09 +0300

    upd

--- Program.cs
+++ Program.cs
@@ -20,4 +20,14 @@
 
         return Math.Sqrt(n);
     }
-}
+    public static int Factorial (int number)
+    {
+        if(number == 1)
+        {
+            return 1;
+        }       
+
+        return number  * Factorial(number - 1);
+
+    }
+}   
